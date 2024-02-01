0e09b5099d17c14af0d45f144c78a15a45df32d7
Elizabeth Wickes <elliewix@gmail.com>
2024-02-01 09:14:39 -0600

    tuesday

---
+++ IS205A2024Spring/week 3/tuesday.py
@@ -0,0 +1,26 @@
+print("line 1 hello")
+print("line 2")
+print()
+# caution!
+print # this is valid, but won't do the thing
+
+####
+
+print("1") # as a string, text
+print("the next will be a number")
+print(1 + 10) # as numbers, with addition
+print("the next will be a string")
+print("1" + "1") # string concatenation
+
+color = "orange"
+domain = "xenobiology"
+number = 5 + 1 # integers
+print("I'm in " + str(number) + " classes. My fav is " + domain)
+print("the color of " + domain + " is " + color)
+
+# a tiny bit of math
+
+worked_this_week = 45
+my_overtime_hours = worked_this_week - 40
+print(my_overtime_hours)
+print()
---
+++ IS205B2024Spring/week 3/tuesday.py
@@ -0,0 +1,27 @@
+print("hello")
+print()
+print # caution, this won't run the func
+
+print("1")
+print(1)
+## operators
+
+print("the next is a string")
+print("1" + "1") # strings concat
+print("the next is an int")
+print(1 + 1) # integers add
+
+## variables
+
+color = "blue"
+domain = "xenobiology"
+number = 17
+
+print("I'm in " + str(number) + " classes.")
+print("The color of " + domain + " is " + color)
+
+## a tiny bit of math
+
+worked_this_week = 45 
+my_overtime_hours = worked_this_week - 40
+print(my_overtime_hours)
